11f9cc17c370742ca917885ea76ab78224bada66
abdsam <abdsam@alu.edu.gva.es>
2026-02-06 17:41:56 +0100

    Guardar

--- src/services/productoService.js
+++ src/services/productoService.js
@@ -22,12 +22,11 @@ export const getAllProducts = async () => {
     console.log("Llamando a la API en:", API_URL); 
     const res = await axios.get(API_URL);
     
-    console.log(" Respuesta cruda del servidor:", res.data); 
+    console.log("Respuesta del servidor:", res.data); 
 
     const rawData = res.data.data || res.data;
 
     if (Array.isArray(rawData)) {
-        console.log(" Es un array, mapeando productos...");
         return rawData.map(mapProductoFromAPI);
     } else {
         console.error(" Lo que llegó NO es un array:", rawData);
@@ -38,7 +37,7 @@ export const getAllProducts = async () => {
     }
 
   } catch (err) {
-    console.error("ERROR :", err);
+    console.error("ERROR CRÍTICO:", err);
     throw new Error("Error al obtener los productos: " + err.message);
   }
 };
@@ -62,12 +61,17 @@ export const getProductById = async (id) => {
  * OJO: Aquí hay que asegurar que enviamos los nombres que el backend quiere.
  */
 export const createProduct = async (productData) => {
+  
   try {
+    
     // Convertimos precio a número por seguridad
     const payload = {
-        ...productData,
-        price: Number(productData.price)
-    };
+    name: productData.nombre,
+    description: productData.descripcion,
+    price: Number(productData.precio),
+    category: productData.categoria,
+    photo: productData.imagen || ''
+  };
 
     const res = await axios.post(API_URL, payload);
     // Devolvemos el producto guardado ya mapeado
